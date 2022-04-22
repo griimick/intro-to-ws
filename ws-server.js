8e7a4f7b122031f06fd2fce5cee1d1d7bc9b8de7
Soumik Pradhan <soumikpradhan14@gmail.com>
2022-04-22 18:44:24 +0530

    update

--- ws-server.js
+++ ws-server.js
@@ -3,6 +3,7 @@ const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ port: 8082 }); // pass in server option if an external server
 
 wss.on('connection', function connection(ws) {
+	console.log("new connection");
 	ws.on('message', function message(data) {
 		console.log('received', data.toString());
 	});
