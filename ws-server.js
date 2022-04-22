7e775ca4920df73e443f33c5e19b45d2863a9538
Soumik Pradhan <soumikpradhan14@gmail.com>
2022-04-22 15:39:37 +0530

    Add wss

--- ws-server.js
+++ ws-server.js
@@ -1,10 +1,10 @@
-import { WebSocketServer } from 'ws';
+const { WebSocketServer } = require('ws');
 
 const wss = new WebSocketServer({ port: 8082 }); // pass in server option if an external server
 
 wss.on('connection', function connection(ws) {
 	ws.on('message', function message(data) {
-		console.log('received: %s', data);
+		console.log('received', data.toString());
 	});
 
 	ws.send('Welcome to my websocket server.');
